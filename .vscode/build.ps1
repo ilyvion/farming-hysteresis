d9cbf95288eafcef6472509f3097f3556fd2ac19
Alexander Krivács Schrøder <alexschrod@gmail.com>
2024-05-21 15:51:36 +0200

    build: improved local build script/behavior

--- .vscode/build.ps1
+++ .vscode/build.ps1
@@ -1,30 +1,37 @@
 $ErrorActionPreference = 'Stop'
 
+$env:RimWorldVersion = $args[0]
 $Configuration = 'Release'
 
-$Target = "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\FarmingHysteresis"
+$VersionTargetPrefix = "D:\RimWorld"
+$VersionTargetSuffix = "Mods\FarmingHysteresis"
+#$MainTarget = "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\FarmingHysteresis"
+$MainTarget = "$VersionTargetPrefix\$env:RimWorldVersion\$VersionTargetSuffix"
+
+#Write-Output $MainTarget
+#return
 
 # $env:RimWorldSteamWorkshopFolderPath = "..\.deps\refs"
 $env:RimWorldSteamWorkshopFolderPath = "C:\Program Files (x86)\Steam\steamapps\workshop\content\294100"
 
 # build dlls
-$env:RimWorldVersion = "1.3"
+$env:RimWorldVersion = $args[0] # "1.3"
 dotnet build --configuration $Configuration .vscode/mod.csproj
 if ($LASTEXITCODE -gt 0) {
     throw "Build failed"
 }
-Start-Sleep -Seconds 0.5
-$env:RimWorldVersion = "1.4"
-dotnet build --configuration $Configuration .vscode/mod.csproj
-if ($LASTEXITCODE -gt 0) {
-    throw "Build failed"
-}
-Start-Sleep -Seconds 0.5
-$env:RimWorldVersion = "1.5"
-dotnet build --configuration $Configuration .vscode/mod.csproj
-if ($LASTEXITCODE -gt 0) {
-    throw "Build failed"
-}
+# Start-Sleep -Seconds 0.5
+# $env:RimWorldVersion = "1.4"
+# dotnet build --configuration $Configuration .vscode/mod.csproj
+# if ($LASTEXITCODE -gt 0) {
+#     throw "Build failed"
+# }
+# Start-Sleep -Seconds 0.5
+# $env:RimWorldVersion = "1.5"
+# dotnet build --configuration $Configuration .vscode/mod.csproj
+# if ($LASTEXITCODE -gt 0) {
+#     throw "Build failed"
+# }
 
 # remove pdbs (for release)
 if ($Configuration -eq "Release") {
@@ -33,18 +40,18 @@ if ($Configuration -eq "Release") {
 }
 
 # remove mod folder
-Remove-Item -Path $Target -Recurse -ErrorAction SilentlyContinue
+Remove-Item -Path $MainTarget -Recurse -ErrorAction SilentlyContinue
 
 # copy mod files
-Copy-Item -Path 1.3 $Target\1.3 -Recurse
-Copy-Item -Path 1.4 $Target\1.4 -Recurse
-Copy-Item -Path 1.5 $Target\1.5 -Recurse
+Copy-Item -Path 1.3 $MainTarget\1.3 -Recurse
+Copy-Item -Path 1.4 $MainTarget\1.4 -Recurse
+Copy-Item -Path 1.5 $MainTarget\1.5 -Recurse
 
-Copy-Item -Path Common $Target\Common -Recurse
-Copy-Item -Path About $Target\About -Recurse -Exclude *.pdn
+Copy-Item -Path Common $MainTarget\Common -Recurse
+Copy-Item -Path About $MainTarget\About -Recurse -Exclude *.pdn
 
-Copy-Item -Path CHANGELOG.md $Target
-Copy-Item -Path LICENSE $Target
-Copy-Item -Path LICENSE.Apache-2.0 $Target
-Copy-Item -Path LICENSE.MIT $Target
-Copy-Item -Path README.md $Target
+Copy-Item -Path CHANGELOG.md $MainTarget
+Copy-Item -Path LICENSE $MainTarget
+Copy-Item -Path LICENSE.Apache-2.0 $MainTarget
+Copy-Item -Path LICENSE.MIT $MainTarget
+Copy-Item -Path README.md $MainTarget
