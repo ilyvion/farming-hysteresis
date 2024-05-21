dfbe2cd7be356fd55cee6a6249e73e596949c6b2
Alexander Krivács Schrøder <alexschrod@gmail.com>
2024-05-21 08:07:25 +0200

    build: update build workflows to project standard

--- .vscode/build.ps1
+++ .vscode/build.ps1
@@ -1,28 +1,39 @@
 $ErrorActionPreference = 'Stop'
 
+$Configuration = 'Release'
+
 $Target = "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\FarmingHysteresis"
+
+# $env:RimWorldSteamWorkshopFolderPath = "..\.deps\refs"
+$env:RimWorldSteamWorkshopFolderPath = "C:\Program Files (x86)\Steam\steamapps\workshop\content\294100"
 
 # build dlls
 $env:RimWorldVersion = "1.3"
-dotnet build .vscode
+dotnet build --configuration $Configuration .vscode/mod.csproj
 if ($LASTEXITCODE -gt 0) {
     throw "Build failed"
 }
 Start-Sleep -Seconds 0.5
 $env:RimWorldVersion = "1.4"
-dotnet build .vscode
+dotnet build --configuration $Configuration .vscode/mod.csproj
 if ($LASTEXITCODE -gt 0) {
     throw "Build failed"
 }
 Start-Sleep -Seconds 0.5
 $env:RimWorldVersion = "1.5"
-dotnet build .vscode
+dotnet build --configuration $Configuration .vscode/mod.csproj
 if ($LASTEXITCODE -gt 0) {
     throw "Build failed"
 }
 
+# remove pdbs (for release)
+if ($Configuration -eq "Release") {
+    Remove-Item -Path .\1.5\Assemblies\FarmingHysteresis.pdb -ErrorAction SilentlyContinue
+    Remove-Item -Path .\1.4\Assemblies\FarmingHysteresis.pdb -ErrorAction SilentlyContinue
+}
+
 # remove mod folder
-Remove-Item -Path $Target -Recurse
+Remove-Item -Path $Target -Recurse -ErrorAction SilentlyContinue
 
 # copy mod files
 Copy-Item -Path 1.3 $Target\1.3 -Recurse
