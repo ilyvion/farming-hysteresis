20d69c6eaee2d68d475449a092965c0c44bd8cb4
Alexander Krivács Schrøder <alexschrod@gmail.com>
2026-07-20 11:47:04 +0200

    chore: remove unused GetTargetOrThrow weak-reference helper

--- Source/FarmingHysteresis/Extensions/WeakReferenceExtensions.cs
+++
@@ -1,8 +0,0 @@
-internal static class WeakReferenceExtensions
-{
-    public static T GetTargetOrThrow<T>(this System.WeakReference<T> weakReference)
-        where T : class =>
-        weakReference.TryGetTarget(out var target)
-            ? target
-            : throw new InvalidOperationException("WeakReference was dead when expected not to be");
-}
